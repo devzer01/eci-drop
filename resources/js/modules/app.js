9b16e4fbe881b8c02b40f8e0ced940b734b3d41c
undefined <nayana@corp-gems.com>
2018-05-20 02:41:50 +0700

    changes

--- resources/js/modules/app.js
+++ resources/js/modules/app.js
@@ -1,16 +1,26 @@
-var handler = window.setInterval(function () {
-    var diff = (endTime.getTime() - (new Date()).getTime()) / 1000;
-    var hours = parseInt(diff / 60 / 60);
-    var minutes = parseInt((diff - (hours * 60 * 60)) / 60);
-    var tminutes = minutes + (hours + 60);
-    var seconds = parseInt(endTime.getTime() - (new Date()).getTime());
-    //- ((hours * 60 * 60 * 1000) + (minutes * 60 * 1000)));
-    $("#el_h1").text(hours);
-    $("#el_m1").text(tminutes);
-    $("#el_s1").text(tsx(parseInt(seconds / 1000)));
-    $("#el_s2").text(seconds % 1000);
-}, 1);
-
 var tsx = function(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
+
+var _deadline = function () {
+    var href = decodeURIComponent(window.location.href);
+    var x = href.lastIndexOf("=") + 1;
+    return href.substr(x);
+};
+
+
+var s1 = document.getElementById("s1");
+var s2 = document.getElementById("s2");
+
+var handler = window.setInterval(function (e) {
+
+    var deadline = new Date(_deadline());
+    var seconds = deadline - Date.now();
+
+    //- ((hours * 60 * 60 * 1000) + (minutes * 60 * 1000)));
+    // !*$("#el_h1").text(hours);
+    // $("#el_m1").text(tminutes);
+    s1.innerText = tsx(parseInt(seconds / 1000));
+    s2.innerText = (seconds % 1000);
+}, 1);
+
